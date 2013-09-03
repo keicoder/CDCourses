c53960ef7897ec5d0be9390ed98f07ef86b02268
jun <hyun2012@gmail.com>
2013-09-03 09:29:33 +0900

    데이터 클래스 생성 스토리보드 생성 AppDelegate.m 수정

---
+++ CDCourses/Course.h
@@ -0,0 +1,19 @@
+//
+//  Course.h
+//  CDCourses
+//
+//  Created by lovejun on 2013. 9. 3..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Course : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * author;
+@property (nonatomic, retain) NSDate * releaseDate;
+
+@end
---
+++ CDCourses/Course.m
@@ -0,0 +1,18 @@
+//
+//  Course.m
+//  CDCourses
+//
+//  Created by lovejun on 2013. 9. 3..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "Course.h"
+
+
+@implementation Course
+
+@dynamic title;
+@dynamic author;
+@dynamic releaseDate;
+
+@end
