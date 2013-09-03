582eb68cb56d5f5a730a4757c6010464ccb4008c
jun <hyun2012@gmail.com>
2013-09-03 09:52:32 +0900

    CoursesTableViewController.h @property (nonatomic, strong) NSManagedObjectContext *managedObjectContext; 참조 생성 (카피, 삭제 등 테이블 뷰에서 context로 많은 작업을 하므로)

---
+++ CDCourses/CoursesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  CoursesTableViewController.h
+//  CDCourses
+//
+//  Created by lovejun on 2013. 9. 3..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoursesTableViewController : UITableViewController
+
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+
+@end
