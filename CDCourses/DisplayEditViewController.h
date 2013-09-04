aaa71ec88d37e9ec2d1bc2737172af615ecab9e2
jun <hyun2012@gmail.com>
2013-09-04 13:53:31 +0900

    DisplayEditViewController 클래스 생성

---
+++ CDCourses/DisplayEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  DisplayEditViewController.h
+//  CDCourses
+//
+//  Created by lovejun on 2013. 9. 4..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DisplayEditViewController : UIViewController
+
+@end
---
+++ CDCourses/DisplayEditViewController.m
@@ -0,0 +1,38 @@
+//
+//  DisplayEditViewController.m
+//  CDCourses
+//
+//  Created by lovejun on 2013. 9. 4..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "DisplayEditViewController.h"
+
+@interface DisplayEditViewController ()
+
+@end
+
+@implementation DisplayEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
